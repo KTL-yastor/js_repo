53bd46439739612a3d2e8bfa6fea52b0743075b1
KTL-yastor <65192722+KTL-yastor@users.noreply.github.com>
2023-11-09 09:58:32 +0100

    zad7

--- zad7/zad7.js
+++ zad7/zad7.js
@@ -1,1 +1,20 @@
-function isHappyNumber()
+
+// seen aby nie wchodzic w nieskonczona petle
+
+function isHappyNumber(x, seen = new Set()) {
+    let sum = 0;
+    let digits = x.toString().split("");
+    digits.forEach((element) => {
+        sum += parseInt(element) * parseInt(element);
+    });
+    if (sum == 1) {
+        return true;
+    } else if (seen.has(sum)) {
+        return false;
+    } else {
+        seen.add(sum);
+        return isHappyNumber(sum, seen);
+    }
+}
+
+console.log(isHappyNumber(19));
